579432207918d047639b5555ebb4725459d029e5
Svilen <svilen.sultanov@alte-leipziger.de>
2020-06-12 14:48:17 +0200

    Showing reports only for the last 2 hours

--- Sources/DSCPullServerWeb/Services/EsentDatabaseRepository.cs
+++ Sources/DSCPullServerWeb/Services/EsentDatabaseRepository.cs
@@ -124,7 +124,8 @@ namespace DSCPullServerWeb.Services {
         #region Report Interface
 
         public IList<Report> GetReports() {
-            List<Report> reports = new List<Report>();
+            List<Report> reportsToReturn = new List<Report>();
+            List<Report> reportsInDatabase = new List<Report>();
 
             using (EsentSession session = new EsentSession(_database)) {
                 session.Open();
@@ -136,33 +137,65 @@ namespace DSCPullServerWeb.Services {
                 if (DatabaseTableExists(sessionId, databaseId, TABLE_STATUS_REPORT)) {
                     Api.OpenTable(sessionId, databaseId, TABLE_STATUS_REPORT, OpenTableGrbit.None, out tableId);
 
-                    foreach (string nodeName in GetNamesNodes().Select(node => node.NodeName).ToList()) {
-                        IDictionary<string, JET_COLUMNID> columnDictionary = Api.GetColumnDictionary(sessionId, tableId);
+                    //Get all columns in the table
+                    IDictionary<string, JET_COLUMNID> columnDictionary = Api.GetColumnDictionary(sessionId, tableId);
 
-                        if (Api.GetTableIndexes(sessionId, tableId).Any(index => index.Name == "StartDateIndex") == false) {
-                            var indexDef = "+NodeName\0+EndTime\0\0";
-                            //Create search index.
-                            Api.JetCreateIndex(sessionId, tableId, "StartDateIndex", CreateIndexGrbit.None, indexDef, indexDef.Length, 100);
-                        }
+                    //Create a search index
+                    if (Api.GetTableIndexes(sessionId, tableId).Any(index => index.Name == "EndTimeIndex") == false) {
+                        var indexKey = "+EndTime\0\0";
+                        JET_INDEXCREATE startDateIndex = new JET_INDEXCREATE {
+                            szKey = indexKey,
+                            cbKey = indexKey.Length,
+                            szIndexName = "EndTimeIndex",
+                        };
 
-                        Api.JetSetCurrentIndex(sessionId, tableId, "StartDateIndex");
+                        Api.JetCreateIndex2(sessionId, tableId, new JET_INDEXCREATE[] { startDateIndex }, 1);
+                    }
 
-                        //Create search keys.
-                        Api.MakeKey(sessionId, tableId, nodeName, Encoding.ASCII, MakeKeyGrbit.NewKey);
+                    Api.JetSetCurrentIndex(sessionId, tableId, "EndTimeIndex");
 
-                        if (Api.TrySeek(sessionId, tableId, SeekGrbit.SeekEQ)) {
-                            DateTime endTime = Api.RetrieveColumnAsDateTime(sessionId, tableId, columnDictionary["EndTime"]).GetValueOrDefault();
+                    //Create search keys.
+                    Api.MakeKey(sessionId, tableId, DateTime.Now.AddHours(-2), MakeKeyGrbit.NewKey);
+
+                    if (Api.TrySeek(sessionId, tableId, SeekGrbit.SeekGE)) {
+                        do {
+                            Report reportRow = new Report() {
+                                Id = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["Id"]),
+                                JobId = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["JobId"]),
+                                NodeName = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["NodeName"]),
+                                IPAddress = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["IPAddress"]),
+                                RerfreshMode = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["RefreshMode"]),
+                                OperationType = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["OperationType"]),
+                                Status = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["Status"]),
+                                RebootRequested = Api.RetrieveColumnAsBoolean(sessionId, tableId, columnDictionary["RebootRequested"]).GetValueOrDefault(),
+                                StartTime = Api.RetrieveColumnAsDateTime(sessionId, tableId, columnDictionary["StartTime"]).GetValueOrDefault(),
+                                EndTime = Api.RetrieveColumnAsDateTime(sessionId, tableId, columnDictionary["EndTime"]).GetValueOrDefault(),
+                                LastModifiedTime = Api.RetrieveColumnAsDateTime(sessionId, tableId, columnDictionary["LastModifiedTime"]).GetValueOrDefault(),
+                                LCMVersion = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["LCMVersion"]),
+                                ConfigurationVersion = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["ConfigurationVersion"]),
+                                ReportFormatVersion = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["ReportFormatVersion"]),
+                                Errors = (List<string>)Api.DeserializeObjectFromColumn(sessionId, tableId, columnDictionary["Errors"]),
+                            };
+
+                            List<string> statusDataList = (List<string>)Api.DeserializeObjectFromColumn(sessionId, tableId, columnDictionary["StatusData"]);
+                            if (statusDataList.Count > 0) {
+                                reportRow.StatusData = statusDataList[0];
+                            };
+                            // Field AdditionalData is only available on WS2016 and WMF 5.1
+                            if (columnDictionary.Keys.Contains("AdditionalData")) {
+                                reportRow.AdditionalData = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["AdditionalData"]);
+                            }
+
+                            reportsInDatabase.Add(reportRow);
+                        } while (Api.TryMovePrevious(sessionId, tableId));
+
+                        foreach (string node in GetNamesNodes().Select(node => node.NodeName).ToArray()) {
+
                             string NotCompliantResources = string.Empty;
                             string status = string.Empty;
 
-                            if (endTime < DateTime.Now.AddHours(-2)) {
-                                status = "No status reported in the last 2 hours!";
-                            } else {
-                                status = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["Status"]);
-                            }
-
-                            List<string> statusData = (List<string>)Api.DeserializeObjectFromColumn(sessionId, tableId, columnDictionary["StatusData"]);
-                            StatusDataElement dataElement = JsonConvert.DeserializeObject<StatusDataElement>(statusData[0]);
+                            Report latestReportForNode = reportsInDatabase.Where(r => r.NodeName == node).OrderByDescending(r => r.EndTime).First();
+                            StatusDataElement dataElement = JsonConvert.DeserializeObject<StatusDataElement>(latestReportForNode.StatusData);
 
                             if (dataElement.ResourcesNotInDesiredState?.Count != null) {
                                 StringBuilder sb = new StringBuilder();
@@ -176,72 +209,43 @@ namespace DSCPullServerWeb.Services {
                                     }
                                 }
 
-                                NotCompliantResources = sb.ToString();
+                                latestReportForNode.NotCompliantResources = sb.ToString();
+                            } else {
+                                latestReportForNode.NotCompliantResources = string.Empty;
                             }
 
-                            Report report = new Report() {
-                                Id = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["Id"]),
-                                JobId = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["JobId"]),
-                                NodeName = nodeName,
-                                IPAddress = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["IPAddress"]),
-                                RerfreshMode = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["RefreshMode"]),
-                                OperationType = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["OperationType"]),
-                                Status = status,
-                                RebootRequested = Api.RetrieveColumnAsBoolean(sessionId, tableId, columnDictionary["RebootRequested"]).GetValueOrDefault(),
-                                StartTime = Api.RetrieveColumnAsDateTime(sessionId, tableId, columnDictionary["StartTime"]).GetValueOrDefault(),
-                                EndTime = Api.RetrieveColumnAsDateTime(sessionId, tableId, columnDictionary["EndTime"]).GetValueOrDefault(),
-                                LastModifiedTime = Api.RetrieveColumnAsDateTime(sessionId, tableId, columnDictionary["LastModifiedTime"]).GetValueOrDefault(),
-                                LCMVersion = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["LCMVersion"]),
-                                ConfigurationVersion = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["ConfigurationVersion"]),
-                                ReportFormatVersion = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["ReportFormatVersion"]),
-                                Errors = (List<string>)Api.DeserializeObjectFromColumn(sessionId, tableId, columnDictionary["Errors"]),
-                                StatusData = statusData[0],
-                                NotCompliantResources = NotCompliantResources
-                            };
-
-                            // Field AdditionalData is only available on WS2016 and WMF 5.1
-                            if (columnDictionary.Keys.Contains("AdditionalData")) {
-                                report.AdditionalData = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["AdditionalData"]);
-                            }
-
-                            reports.Add(report);
-                        } else {
-                            Report report = new Report() {
-                                Id = string.Empty,
-                                JobId = string.Empty,
-                                NodeName = nodeName,
-                                IPAddress = string.Empty,
-                                RerfreshMode = string.Empty,
-                                OperationType = string.Empty,
-                                Status = "No status repot found for that server!",
-                                RebootRequested = false,
-                                StartTime = null,
-                                EndTime = null,
-                                LastModifiedTime = null,
-                                LCMVersion = string.Empty,
-                                ConfigurationVersion = string.Empty,
-                                ReportFormatVersion = string.Empty,
-                                Errors = new List<string>() { string.Empty },
-                                StatusData = string.Empty,
-                                NotCompliantResources = string.Empty
-                            };
-
-                            // Field AdditionalData is only available on WS2016 and WMF 5.1
-                            if (columnDictionary.Keys.Contains("AdditionalData")) {
-                                report.AdditionalData = Api.RetrieveColumnAsString(sessionId, tableId, columnDictionary["AdditionalData"]);
-                            }
-
-                            reports.Add(report);
+                            reportsToReturn.Add(latestReportForNode);
                         }
-                    }
+                    } else {
+                        Report report = new Report() {
+                            Id = string.Empty,
+                            JobId = string.Empty,
+                            NodeName = null,
+                            IPAddress = string.Empty,
+                            RerfreshMode = string.Empty,
+                            OperationType = string.Empty,
+                            Status = "No status repot found for the last 2 hours!",
+                            RebootRequested = false,
+                            StartTime = null,
+                            EndTime = null,
+                            LastModifiedTime = null,
+                            LCMVersion = string.Empty,
+                            ConfigurationVersion = string.Empty,
+                            ReportFormatVersion = string.Empty,
+                            Errors = new List<string>() { string.Empty },
+                            StatusData = string.Empty,
+                            NotCompliantResources = string.Empty
+                        };
 
+                        reportsToReturn.Add(report);
+                    }
                     Api.JetCloseTable(sessionId, tableId);
                 } else {
                     _logger.Log(10091, String.Format("Database table {0} not found!", TABLE_STATUS_REPORT), LogLevel.Warning);
                 }
             }
 
-            return reports.OrderBy(r => r.StartTime).ToList();
+            return reportsToReturn.OrderBy(r => r.StartTime).ToList();
         }
 
         #endregion
